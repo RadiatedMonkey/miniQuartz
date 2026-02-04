RadiatedMonkey/miniQuartz

commit 5367ed2768f025fcb495f0de4b9ff1cb0a36ff53

Songs added to playlist now appear immediately

playlists/0000new playlist.m3u
@@ -1,1 +1,5 @@
 #EXTM3U
+#EXTINF:0?Crocodile Moat!!!!!!!!?Curta'n Wall?cache/cover_15488932226818620559.jpg?Crocodile Moat!!!!!!!!
+.././playlists/playlist-1\Crocodile Moat!!!!!!!!.mp3
+#EXTINF:0?Downtempo.mp3?Unknown Artist?assets/icon-256.png?Unknown Album
+.././playlists/playlist-1\Downtempo.mp3

playlists/0004new playlist.m3u
@@ -1,0 +1,1 @@
+#EXTM3U

src/app.rs
@@ -117,6 +117,7 @@ fn add_to_playlist(
         &format!("{}", new_song.cover_path),
         &new_song.album,
     );
+
     let _ = write_m3u(file_path, &playlist, false, true, false);
     Ok(())
 }
@@ -455,6 +456,7 @@ impl TemplateApp {
                 let playlist_path = path_to_string(&playlist.to_path_buf());
                 if ui.button(&playlist_name).clicked() {
                     let _ = add_to_playlist(&playlist_path, &song_data);
+                    self.songs.articles.push(song_data.clone()); // wish this could be in the add_to_playlist function but couldn't get it to play nice. skill issue
                     if Some(playlist_name.clone()) == self.currently_selected_playlist {
                         self.songs.articles.extend([song_data.clone()]);
                     }
@@ -1235,7 +1237,8 @@ impl eframe::App for TemplateApp {
                                         .unwrap()
                                         .to_path_buf(),
                                 );
-                                let _ = edit_m3u_track( // todo: actual error handling
+                                let _ = edit_m3u_track(
+                                    // todo: actual error handling
                                     &playlist_path,
                                     index,
                                     song.album.clone(),

meow3.mcu
@@ -1,3 +1,0 @@
-#EXTM3U
-#EXTINF:0,Color Bars
-./playlists/playlist-1/color bars.mp3

playlists/0001meow.m3u
@@ -3,8 +3,6 @@
 ../playlists/playlist-1\Childs Play.mp3
 #EXTINF:0?Child's Play?Alex G?assets/icon-256.png?Unknown Album
 ../playlists/playlist-1\Childs Play.mp3
-#EXTINF:0?Crocodile Moat!!!!!!!!?Curta'n Wall?cache/cover_15488932226818620559.jpg?Crocodile Moat!!!!!!!!
-../playlists/playlist-1\Crocodile Moat!!!!!!!!.mp3
 #EXTINF:0?Downtempo.mp3?Unknown Artist?assets/icon-256.png?Unknown Album
 ../playlists/playlist-1\Downtempo.mp3
 #EXTINF:0?fallen 4 Ü?100 Gecs?cache/cover_6456464390098348098.jpg?Unreleased
@@ -27,3 +25,7 @@
 ../playlists/playlist-1\Crocodile Moat!!!!!!!!.mp3
 #EXTINF:0?Downtempo.mp3?Unknown Artist?assets/icon-256.png?Unknown Album
 ../playlists/playlist-1\Downtempo.mp3
+#EXTINF:0?Downtempo.mp3?Unknown Artist?assets/icon-256.png?Unknown Album
+.././playlists/playlist-1\Downtempo.mp3
+#EXTINF:0?Downtempo.mp3?Unknown Artist?assets/icon-256.png?Unknown Album
+../././playlists/playlist-1\Downtempo.mp3

playlists/0003new playlist.m3u
@@ -1,0 +1,1 @@
+#EXTM3U

commit 4d8c7610ffac168f7b9bead0cf6ac7bf113b854c

Fixed small playlist add bug introduced by commit 5367ed2768f025fcb495f0de4b9ff1cb0a36ff53 Bug was: When adding song to a playlist that isn't currently selected, it would appear in hte currently selected playlist until reloaded.

playlists/0000new playlist.m3u
@@ -1,1 +1,7 @@
 #EXTM3U
+#EXTINF:0?iFeelLikeYouWould?xXHANA VENOMXx?cache/cover_15011064853867461280.jpg?920LONDON
+../playlists/playlist-1\xXHANA VENOMXx - 920LONDON - 02 iFeelLikeYouWould.flac
+#EXTINF:0?iFeelLikeYouWould?xXHANA VENOMXx?cache/cover_15011064853867461280.jpg?920LONDON
+.././playlists/playlist-1\xXHANA VENOMXx - 920LONDON - 02 iFeelLikeYouWould.flac
+#EXTINF:0?genderfluid lesbians?YASHEXTRA?cache/cover_5797866118481501825.jpg?genderfluid lesbians
+../playlists/playlist-1\YASHEXTRA - genderfluid lesbians2.mp3

playlists/0002new playlist.m3u
@@ -1,1 +1,15 @@
 #EXTM3U
+#EXTINF:0?LOTTERY?Femtanyl?cache/cover_17399758085745754865.jpg?LOTTERY
+../playlists/playlist-1\01 ☆ LOTTERY.flac
+#EXTINF:0?see u soon?abjectdog?cache/cover_17209476647860144101.jpg?2024
+../playlists/playlist-1\abjectdog - see u soon.mp3
+#EXTINF:0?Child's Play?Alex G?assets/icon-256.png?Unknown Album
+../playlists/playlist-1\Childs Play.mp3
+#EXTINF:0?color bars?glass beach?cache/cover_12913935347169404221.jpg?unreleased
+../playlists/playlist-1\color bars.mp3
+#EXTINF:0?Crocodile Moat!!!!!!!!?Curta'n Wall?cache/cover_15488932226818620559.jpg?Crocodile Moat!!!!!!!!
+../playlists/playlist-1\Crocodile Moat!!!!!!!!.mp3
+#EXTINF:0?Downtempo.mp3?Unknown Artist?assets/icon-256.png?Unknown Album
+../playlists/playlist-1\Downtempo.mp3
+#EXTINF:0?Electric Fan Harp in the bathhouse?Electronicos Fantasticos?assets/icon-256.png?Unknown Album
+../playlists/playlist-1\Electric Fan Harp in the bathhouse.mp3

playlists/0004new playlist.m3u
@@ -1,15 +1,7 @@
 #EXTM3U
-#EXTINF:0?LOTTERY?Femtanyl?cache/cover_17399758085745754865.jpg?LOTTERY
-../playlists/playlist-1\01 ☆ LOTTERY.flac
+#EXTINF:0?see u soon?abjectdog?cache/cover_17209476647860144101.jpg?2024
+.././playlists/playlist-1\abjectdog - see u soon.mp3
 #EXTINF:0?see u soon?abjectdog?cache/cover_17209476647860144101.jpg?2024
 ../playlists/playlist-1\abjectdog - see u soon.mp3
-#EXTINF:0?Child's Play?Alex G?assets/icon-256.png?Unknown Album
-../playlists/playlist-1\Childs Play.mp3
-#EXTINF:0?color bars?glass beach?cache/cover_12913935347169404221.jpg?unreleased
-../playlists/playlist-1\color bars.mp3
-#EXTINF:0?Crocodile Moat!!!!!!!!?Curta'n Wall?cache/cover_15488932226818620559.jpg?Crocodile Moat!!!!!!!!
-../playlists/playlist-1\Crocodile Moat!!!!!!!!.mp3
-#EXTINF:0?Downtempo.mp3?Unknown Artist?assets/icon-256.png?Unknown Album
-../playlists/playlist-1\Downtempo.mp3
-#EXTINF:0?Electric Fan Harp in the bathhouse?Electronicos Fantasticos?assets/icon-256.png?Unknown Album
-../playlists/playlist-1\Electric Fan Harp in the bathhouse.mp3
+#EXTINF:0?see u soon?abjectdog?cache/cover_17209476647860144101.jpg?2024
+../playlists/playlist-1\abjectdog - see u soon.mp3

src/app.rs
@@ -452,12 +452,14 @@ impl TemplateApp {
 
         ui.menu_button("Add to playlist", |ui| {
             for playlist in &self.playlists {
-                let playlist_name = path_to_string_name(playlist);
+                let playlist_name = &path_to_string_name(playlist)[4..];
                 let playlist_path = path_to_string(&playlist.to_path_buf());
-                if ui.button(&playlist_name).clicked() {
+                if ui.button(playlist_name).clicked() {
                     let _ = add_to_playlist(&playlist_path, &song_data);
-                    self.songs.articles.push(song_data.clone()); // wish this could be in the add_to_playlist function but couldn't get it to play nice. skill issue
-                    if Some(playlist_name.clone()) == self.currently_selected_playlist {
+                    if (Some(playlist_path) == self.currently_selected_playlist) {
+                        self.songs.articles.push(song_data.clone()); // wish this could be in the add_to_playlist function but couldn't get it to play nice. skill issue
+                    }
+                    if Some(playlist_name) == self.currently_selected_playlist.as_deref() {
                         self.songs.articles.extend([song_data.clone()]);
                     }
                 }
@@ -1098,7 +1100,9 @@ impl eframe::App for TemplateApp {
 
                     if ui.selectable_label(false, "+").clicked() {
                         let count = to_base62(self.playlists.len() + 1, 4);
-                        if let Err(error) = create_empty_m3u(&format!("./playlists/{}new playlist.m3u", count)){
+                        if let Err(error) =
+                            create_empty_m3u(&format!("./playlists/{}new playlist.m3u", count))
+                        {
                             show_error(self, error.to_string());
                         }
                         self.playlists = get_playlists("./playlists/").unwrap_or_default();
@@ -1122,8 +1126,7 @@ impl eframe::App for TemplateApp {
                                     self.playlists[count] = playlist.clone(); // this should probably be on a different thread, since a huge amount of playlists will cause a freeze bc disk operations
                                     let _ = &playlist.set_extension("m3utmp");
 
-                                    if let Err(error) = fs::rename(&old_path, &playlist)
-                                    {
+                                    if let Err(error) = fs::rename(&old_path, &playlist) {
                                         show_error(
                                             self,
                                             format!(
@@ -1140,8 +1143,7 @@ impl eframe::App for TemplateApp {
                                     let mut old_path = playlist.clone();
                                     old_path.set_extension("m3utmp");
                                     let _ = &playlist.set_extension("m3u");
-                                    if let Err(error) = fs::rename(&old_path, &playlist)
-                                    {
+                                    if let Err(error) = fs::rename(&old_path, &playlist) {
                                         show_error(
                                             self,
                                             format!(